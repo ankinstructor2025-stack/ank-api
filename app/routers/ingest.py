393a71d8ba622c0531e09bb3fc688d2044f48363
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-03-01 16:24:13 +0900

    Delete app/routers/ingest.py

--- app/routers/ingest.py
+++
@@ -1,9 +0,0 @@
-# app/routers/ingest.py
-
-from fastapi import APIRouter
-
-from routers.ingest.kokkai_test import router as kokkai_router
-
-router = APIRouter(prefix="/ingest", tags=["ingest"])
-
-router.include_router(kokkai_router)
