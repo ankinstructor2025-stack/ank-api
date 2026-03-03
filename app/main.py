e7186b14558348020a46a50c36903682bf8ccd26
ankinstructor2025-stack <ank.instructor2025@gmail.com>
2026-03-03 19:22:29 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.routers.jma_test import router as jma_router
 from app.routers.egov_test import router as egov_router
 from app.routers.caa_test import router as caa_router
 from app.routers.tokyo_test import router as tokyo_router
+from app.routers.upload_and_register import router as upload_and_register
 
 def create_app() -> FastAPI:
     app = FastAPI()
@@ -25,6 +26,7 @@ def create_app() -> FastAPI:
     app.include_router(egov_router, prefix="/v1")
     app.include_router(caa_router, prefix="/v1")
     app.include_router(tokyo_router, prefix="/v1")
+    app.include_router(upload_and_register, prefix="/v1")
     return app
 
 app = create_app()
